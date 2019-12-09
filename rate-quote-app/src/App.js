335ce6599ae3af0bbf6bea3eb30d087d78d1bdc6
allison-strandberg <allisonstrandberg@gmail.com>
2019-12-08 23:51:51 -0500

    Add a controlled form using local component state
    Why:
    
    * Form input should be stored in state, but dispatching state updates to the
    Redux store while the user is typing may hurt performance.
    
    This change addresses the need by:
    
    * Render a form in the App component, with input fields and submit
    button
    * When an input field value is changed, update App component state
    * Test that fields render and initial state is set properly

--- rate-quote-app/src/App.js
+++ rate-quote-app/src/App.js
@@ -2,25 +2,69 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loanSize: null,
+      propertyType: "SingleFamily",
+      creditScore: null,
+      occupancy: "Primary",
+    }
+  }
+
+  handleInputChange = event => {
+    this.setState({ [event.target.id]: event.target.value});
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <label htmlFor="loan-size">Loan Size</label>
+          <input 
+            id="loanSize" 
+            type="text" 
+            onChange={this.handleInputChange} 
+          />
+          <label htmlFor="property-type">Property Type</label>
+          <select 
+            id="propertyType"
+            onChange={this.handleInputChange}
+          >
+            <option value="SingleFamily">Single Family</option>
+            <option value="Condo">Condo</option>
+            <option value="Townhouse">Townhouse</option>
+            <option value="MultiFamily">Multi Family</option>
+          </select>
+          <label htmlFor="credit-score">Credit Score</label>
+          <input 
+            id="creditScore" 
+            type="text"
+            onChange={this.handleInputChange}  
+          />
+          <label htmlFor="occupancy">Occupancy</label>
+          <select 
+            id="occupancy"
+            onChange={this.handleInputChange}
+          >
+            <option value="Primary">Primary</option>
+            <option value="Secondary">Secondary</option>
+            <option value="Investment">Investment</option>
+          </select>
+          <button 
+            type="submit" 
+            onClick={this.handleSubmit} 
+          >Quote Rates
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
--- rate-quote-app/src/App.test.js
+++ rate-quote-app/src/App.test.js
@@ -1,9 +1,45 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { configure, shallow, mount }  from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+describe('App component', () => {
+	it('should render two input elements', function() {
+		const wrapper = shallow(<App/>);
+		expect(wrapper.find('input')).to.have.length(2);
+	});
+
+	it('should render two select elements', function() {
+		const wrapper = shallow(<App/>);
+		expect(wrapper.find('select')).to.have.length(2);
+	});
+
+	it('should render one button element', function() {
+	 	const wrapper = shallow(<App/>);
+	 	expect(wrapper.find('button')).to.have.length(1);
+	});
+
+	it('should have an initial state of loan size of null', function() {
+	 	const wrapper = shallow(<App/>);
+	 	expect(wrapper.instance().state.loanSize).to.be.null;
+	});
+
+	it('should have an initial state of credit score of null', function() {
+	 	const wrapper = shallow(<App/>);
+	 	expect(wrapper.instance().state.creditScore).to.be.null;
+	});
+
+	it('should have an initial state of property type of "SingleFamily"', function() {
+	 	const wrapper = shallow(<App/>);
+	 	expect(wrapper.instance().state.propertyType).to.equal('SingleFamily');
+	});
+
+	it('should have an initial state of occupancy of "Primary"', function() {
+	 	const wrapper = shallow(<App/>);
+	 	expect(wrapper.instance().state.occupancy).to.equal('Primary');
+	});
+
 });

ed3a012ca372084c981bfa56dd4ab1e8688416e5
allison-strandberg <allisonstrandberg@gmail.com>
2019-12-23 10:53:50 -0500

    Format table results
    Why:
    
    * The numbers returned from the API must be formatted to display: add
    percent and dollar signs and terminate numbers at three decimals.
    
    This change addresses the need by:
    
    * Transform API values in quotes reducer.

--- rate-quote-app/src/redux/reducers/quotes.js
+++ rate-quote-app/src/redux/reducers/quotes.js
@@ -16,6 +16,12 @@ export const quotes = (state = {
 			break
 		case RECEIVE_QUOTES:
 		case RECEIVE_FAILURE:
+			action.payload.rateQuotes.map(quote => {
+				quote.interestRate = `${quote.interestRate.toFixed(3)}%`;
+				quote.closingCosts = `$${quote.closingCosts.toFixed()}`;
+				quote.monthlyPayment = `$${quote.monthlyPayment.toFixed()}`;
+				quote.apr = `${quote.apr.toFixed(3)}%`;
+			});
 			return Object.assign({}, state, action.payload);
 			break
 		default:
